8645e51179b1ada178f27a56a2cdfbeb0ea43c75
Zack Puhl <github@xmit.xyz>
2026-03-16 17:56:16 +0000

    wip: the filters work! just need to clean things up and implement some more related int tests

--- run-tests.sh
+++ run-tests.sh
@@ -6,4 +6,17 @@
 TEST_OPTS="-O${1:-0} --libdir .. --lib bloom"
 
 echo -e "\n===== Running unit tests..."
-c3c compile-test ${TEST_OPTS} ./test/ || { echo "Unit tests failed. Aborting." && exit 1; }
+c3c compile-test ${TEST_OPTS} ./test/unit/ || { echo "Unit tests failed. Aborting." && exit 1; }
+
+if [[ ! -d test/int/artifacts ]]; then
+	tar zxvf ./test/int/artifacts.tgz || { echo "Failed to extract artifacts tarball. Aborting." && exit 1; }
+fi
+echo -e "\n\n\n===== Running integration tests..."
+# note: set env var NON_INTERACTIVE=yes to forego interacting with these test applications
+for x in `find ./test/int/ -maxdepth 1 -type f -name '*.c3'`; do
+	c3c compile-run ${TEST_OPTS} $x -- "${NON_INTERACTIVE:-yes}" \
+		|| { echo "Integration test '$(basename "$x")' failed. Aborting." && exit 1; }
+done
+
+# TODO: Add `generate` tests to create Bloom filters from different inputs
+# TODO: Add `query` tests to read raw Bloom filter and immediately enable searching the text for 1- to 3-word phrases
